04cb10f01a10fdd7a9bfec0bc9ac1b5bb9b9bd37
xrrocha <xrrocha@977266c8-e2c3-11dd-962d-ef4e56be9935>
2009-01-15 22:52:03 +0000

    Removed old component factory package

--- phoenix-core/src/main/java/com/sun/phoenix/components/factory/ObjectFactory.java
+++
@@ -1,6 +0,0 @@
-package com.sun.phoenix.components.factory;
-
-public interface ObjectFactory<T> {
-    public T newInstance() throws Exception;
-    public void restoreTransientProperties(T element) throws Exception;
-}
